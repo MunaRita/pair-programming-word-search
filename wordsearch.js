79d8a1fb3acbd19ea4fffb70133b34af83fd3d20
vagrant <chiomaanierobi@gmail.com>
2021-07-01 13:42:30 +0000

    Refactoring

--- wordsearch.js
+++ wordsearch.js
@@ -1,33 +1,35 @@
 const transpose = function(matrix) {
-    const newMatrix = [];
-    for(let i = 0; i < matrix[0].length; i++) {
-      newMatrix.push([]);
+  const newMatrix = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    newMatrix.push([]);
+  }
+   
+  for (let row = 0; row < matrix.length; row++) {
+      
+    for (let column = 0; column < matrix[row].length; column++) {
+      newMatrix[column][row] = matrix[row][column];
     }
-    //console.log(newMatrix, "logging after pushing empty array");
-    for (let row = 0; row < matrix.length; row++) {
-      //push another array into matrix
-      for (let column = 0; column < matrix[row].length; column++) {
-        newMatrix[column][row] = matrix[row][column];
-      }
-    } return newMatrix;
+  } return newMatrix;
 };
+
 const wordSearch = (letters, word) => {
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) {
-            return true;
-        }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-    const newLetters = transpose(letters);
-    const vertJoin = newLetters.map(ls => ls.join(''))
-    for (l of vertJoin) {
-        if (l.includes(word)) {
-           return true;
-        }
+  }
+  const newLetters = transpose(letters);
+  const vertJoin = newLetters.map(ls => ls.join(''));
+  for (let l of vertJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-    return false
-}
-module.exports = wordSearch
+  }
+  return false;
+};
+
+module.exports = wordSearch;
 
 
 
